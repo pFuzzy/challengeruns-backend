03f383ef50a33ffc967f224f8ff0a692d9858d86
pFuzzy <p.fazii888@gmail.com>
2020-11-13 14:40:08 -0800

    Initial commit

---
+++ src/main/java/com/codecool/challengerunsbackend/ChallengeRunsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.challengerunsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeRunsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeRunsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/challengerunsbackend/ChallengeRunsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.challengerunsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeRunsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
