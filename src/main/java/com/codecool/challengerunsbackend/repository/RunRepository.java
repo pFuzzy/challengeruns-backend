7cd456d88377c52bb016d2cd537271cdc7c0d092
pFuzzy <p.fazii888@gmail.com>
2020-11-14 08:16:40 -0800

    add option to get run by category

--- src/main/java/com/codecool/challengerunsbackend/repository/RunRepository.java
+++ src/main/java/com/codecool/challengerunsbackend/repository/RunRepository.java
@@ -4,4 +4,5 @@ import com.codecool.challengerunsbackend.entity.Run;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RunRepository extends JpaRepository<Run, Long> {
+    Run getRunByCategory(String category);
 }
--- src/test/java/com/codecool/challengerunsbackend/repository/RunRepositoryTest.java
+++ src/test/java/com/codecool/challengerunsbackend/repository/RunRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.codecool.challengerunsbackend.repository;
 
 import com.codecool.challengerunsbackend.entity.Run;
+import com.codecool.challengerunsbackend.entity.Split;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,34 @@ public class RunRepositoryTest {
         Run testRun = Run.builder().build();
 
         runRepository.save(testRun);
+    }
+
+    @Test
+    public void getRunByCategory(){
+        Split testSplit1 = Split.builder()
+                .name("Asylum demon")
+                .build();
+        Split testSplit2 = Split.builder()
+                .name("Quelaag")
+                .build();
+        Split testSplit3 = Split.builder()
+                .name("Bell gargoyles")
+                .build();
+        Run testRun = Run.builder()
+                .category("Any%")
+                .split(testSplit1)
+                .split(testSplit2)
+                .split(testSplit3)
+                .build();
+
+        testSplit1.setRun(testRun);
+        testSplit2.setRun(testRun);
+        testSplit3.setRun(testRun);
+
+        runRepository.save(testRun);
+
+        assertThat(runRepository.getRunByCategory("Any%").equals(testRun));
+        assertThat(runRepository.getRunByCategory("Any%").getSplits().equals(Lists.newArrayList(testSplit1,testSplit2,testSplit3)));
     }
 
 }
