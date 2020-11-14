1702b743d7369615ef3d5357dac87fd0f940ba98
pFuzzy <p.fazii888@gmail.com>
2020-11-14 06:03:58 -0800

    add game and run repositories

---
+++ src/main/java/com/codecool/challengerunsbackend/repository/RunRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.challengerunsbackend.repository;
+
+import com.codecool.challengerunsbackend.entity.Run;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RunRepository extends JpaRepository<Run, Long> {
+}
