2df9633ca950f2b837b430502f395be8e3483eca
pFuzzy <p.fazii888@gmail.com>
2020-11-21 14:13:32 -0800

    add getGameById and id specific route

--- src/main/java/com/codecool/challengerunsbackend/controller/GameController.java
+++ src/main/java/com/codecool/challengerunsbackend/controller/GameController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +22,10 @@ public class GameController {
     @GetMapping("/games")
     public ResponseEntity<List<Game>> findAllGames(){
         return ResponseEntity.ok(gameRepository.findAll());
+    }
+
+    @GetMapping("/games/{id}")
+    public ResponseEntity<Game> getGameById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(gameRepository.getGameById(id));
     }
 }
--- src/main/java/com/codecool/challengerunsbackend/repository/GameRepository.java
+++ src/main/java/com/codecool/challengerunsbackend/repository/GameRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
     Game getGameByTitle(String title);
+
+    Game getGameById(Long id);
 }
