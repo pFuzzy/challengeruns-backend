1f9338b15802d96914e519c95eeb72b2a7c03ab5
pFuzzy <p.fazii888@gmail.com>
2020-12-28 14:53:23 +0100

    create challengerun user entity

--- src/main/java/com/codecool/challengerunsbackend/controller/AuthController.java
+++ src/main/java/com/codecool/challengerunsbackend/controller/AuthController.java
@@ -1,15 +1,15 @@
 package com.codecool.challengerunsbackend.controller;
 
+import com.codecool.challengerunsbackend.entity.CrUser;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 public class AuthController {
     @PostMapping("/register")
-    public ResponseEntity<?> register(){
+    public ResponseEntity<?> register(@RequestBody CrUser crUser){
+        System.out.println(crUser.toString());
         return ResponseEntity.ok("Ok");
     }
 }
---
+++ src/main/java/com/codecool/challengerunsbackend/entity/CrUser.java
@@ -0,0 +1,27 @@
+package com.codecool.challengerunsbackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CrUser {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
