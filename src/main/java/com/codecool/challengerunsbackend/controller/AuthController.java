d847ced6d154d0ddc68b72f4eabe0276194d5565
pFuzzy <p.fazii888@gmail.com>
2020-12-28 15:30:38 +0100

    add user data checking and password encoding

--- src/main/java/com/codecool/challengerunsbackend/controller/AuthController.java
+++ src/main/java/com/codecool/challengerunsbackend/controller/AuthController.java
@@ -4,19 +4,31 @@ import com.codecool.challengerunsbackend.entity.CrUser;
 import com.codecool.challengerunsbackend.repository.CrUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 public class AuthController {
 
+    private final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
     @Autowired
     private CrUserRepository userRepository;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody CrUser crUser){
-        userRepository.save(crUser);
+    public ResponseEntity<?> register(@RequestBody CrUser user) {
+        String response = "Registration successful";
+        if (userRepository.existsCrUserByUsername(user.getUsername())) {
+            response = "Username already exists!";
+        } else if (userRepository.existsCrUserByEmail(user.getEmail())) {
+            response = "E-mail already in use";
+        } else {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+        }
         System.out.println(userRepository.findAll());
-        return ResponseEntity.ok("Ok");
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/codecool/challengerunsbackend/repository/CrUserRepository.java
+++ src/main/java/com/codecool/challengerunsbackend/repository/CrUserRepository.java
@@ -4,4 +4,9 @@ import com.codecool.challengerunsbackend.entity.CrUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrUserRepository extends JpaRepository<CrUser, Long> {
+    boolean existsCrUserByUsername(String username);
+
+    boolean existsCrUserByEmail(String email);
+
+
 }
