c380f8273dc734cd112decaf86721b0441492f8c
pFuzzy <p.fazii888@gmail.com>
2021-02-18 15:54:57 +0100

    modify crUser class and repository for login

--- src/main/java/com/codecool/challengerunsbackend/entity/CrUser.java
+++ src/main/java/com/codecool/challengerunsbackend/entity/CrUser.java
@@ -1,15 +1,19 @@
 package com.codecool.challengerunsbackend.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.assertj.core.util.Lists;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 public class CrUser {
     @Id
     @GeneratedValue
@@ -23,5 +27,9 @@ public class CrUser {
 
     @Column(nullable = false)
     private String password;
+
+    @ElementCollection
+    @Builder.Default
+    private List<String> roles = Lists.newArrayList("ROLE_USER");
 
 }
